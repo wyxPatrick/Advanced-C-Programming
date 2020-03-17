4bd44aa6c09f2ef23db33379ffb4e7578ad5be05
Youxing Wang <wyxpatrick@gmail.com>
2020-03-17 17:41:04 -0400

    Week 9

---
+++ C++-Programming/Week-9/Project2/Project2/Source1.cpp
@@ -0,0 +1,16 @@
+template<class T>
+class CMyistream_iterator {
+private:
+    T content;
+    istream& is;
+public:
+    CMyistream_iterator(istream& i) :is(i) {
+        i >> content;
+    }
+    T operator*() {
+        return content;
+    }
+    void operator++(int) {
+        is >> content;
+    }
+};
